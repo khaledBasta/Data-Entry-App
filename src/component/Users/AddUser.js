c4c83b78dafd6f1931ff8cb95884e03346997c5e
khaled basta <khaled.basta@protonmail.com>
2022-06-09 06:13:02 +0200

    Use Refs instead of useState

--- src/component/Users/AddUser.js
+++ src/component/Users/AddUser.js
@@ -1,23 +1,32 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useRef } from "react";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  // Where u just only want to quickly read a value, and u never plan on changing anything,
+  // Then u don't really need a state, "refs are probably better"
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  // const [enteredUsername, setEnteredUsername] = useState("");
+  // const [enteredAge, setEnteredAge] = useState("");
   const [throwError, setThrowError] = useState(false);
 
   const addUserHanlder = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    console.log(enteredName, enteredUserAge);
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setThrowError({
         title: "Invalid input",
         message: "Please enter a vaild name and age.",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setThrowError({
         title: "Invalid input",
         message: "Please enter an age bigger than 0.",
@@ -25,22 +34,29 @@ const AddUser = (props) => {
       return;
     }
 
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
+    props.onAddUser(enteredName, enteredUserAge);
+
+    // U can manipulate the DOM without React but it is not prefered
+    // Reset the value entered by a user using refs which is a less code
+    // Or using state, which is definitely cleaner but is a bit more code
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+    // setEnteredUsername("");
+    // setEnteredAge("");
   };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
+  // const usernameChangeHandler = (event) => {
+  //   setEnteredUsername(event.target.value);
+  // };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const errorHanlder = () => {
     setThrowError(null);
   };
+
   return (
     <Fragment>
       {throwError && (
@@ -54,17 +70,19 @@ const AddUser = (props) => {
         <form onSubmit={addUserHanlder}>
           <label htmlFor="username">Username</label>
           <input
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            // value={enteredUsername}
+            // onChange={usernameChangeHandler}
             id="username"
             type="text"
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            // value={enteredAge}
+            // onChange={ageChangeHandler}
             id="age"
             type="number"
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
